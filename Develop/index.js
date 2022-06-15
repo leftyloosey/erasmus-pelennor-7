bbb4adecd6a20e3cb9aceb5b50b6825c816c704a
davidhardin <thisgoestodavid@gmail.com>
2022-06-15 13:51:49 -0600

    questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: "Title of README?",
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: "Describe it:",
+    },
+    {
+    type: 'input',
+    name: 'contents',
+    message: "Table of Contents:",
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: "Installation instructions:",
+    },
+    {
+    type: 'input',
+    name: 'usage',
+    message: "Usage Information:",
+    },
+    {
+    type: 'input',
+    name: 'contribute',
+    message: "Contribution Guidelines:",
+    },
+    {
+    type: 'input',
+    name: 'test',
+    message: "Testing Instructions:",
+    }
+]
+
+  inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '))
+  })
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
